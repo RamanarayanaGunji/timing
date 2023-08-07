2e5a6e0fe3a2b71de6ad3cc4f1b5ccaef62299c1
Ramanarayana <gunjirama1999@gmail.com>
2023-08-08 05:23:40 +0530

    timing creation

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+    cidr ="10.0.0.0/16"
+    public_subnet_cidr = "10.0.0.0/24"
+    tags={
+    name = "timing"
+    environment = "dev"
+    terraform = "true"
+    }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+    variable "cidr"{
+    type = string
+    #default ="10.0.0.0/16"
+
+    }
+    variable "public_subnet_cidr"{
+    type = string
+    }
+    variable "tags"{
+    type = map
+    }
