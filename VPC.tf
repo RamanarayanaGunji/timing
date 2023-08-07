ae615cabf8388c26239cff150322de264f863e8b
Ramanarayana <gunjirama1999@gmail.com>
2023-08-08 05:27:29 +0530

    timing creation

--- VPC.tf
+++ VPC.tf
@@ -6,19 +6,19 @@
 
 }
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.public.id
   cidr_block = var.public_subnet_cidr
 
   tags = var.tags
 }
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.public.id
   cidr_block = "10.0.2.0/24"
 
   tags = var.tags
 }
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.public.id
 
   tags = var.tags
 }
